0a6806aa0e9a24cea2f1b45d9b04c1d1e9ac9350
mohammed-albahlool <100028748+mohammed-albahlool@users.noreply.github.com>
2023-06-24 16:33:47 +0300

    new updated

--- lib/main.dart
+++ lib/main.dart
@@ -13,6 +13,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+
+
       debugShowCheckedModeBanner: false,
       initialRoute: '/launch_screen',
           routes: {
